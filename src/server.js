c3c24e70eb7593b418be2a6ac8c783b466d4231c
Dustin Brickwood <dustinbrickwood204@gmail.com>
2018-02-10 15:56:16 -0500

    vision api working

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 const express = require('express');
-const vision  = require('@google-cloud/vision');
+const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient();
-const cors    = require('cors');
-const PORT    = process.env.PORT || 8080 
-const admin   = require("firebase-admin")
+const cors = require('cors');
+const PORT = process.env.PORT || 8080
+const admin = require("firebase-admin")
+const axios = require('axios');
 const deploy = require('./keys.js');
 const serviceAccount = deploy.firepack;
 
 app = express()
+app.use(express.static('public'))
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -18,23 +20,23 @@ admin.initializeApp({
     databaseURL: "https://fishdex-hackathon.firebaseio.com"
 });
 
-app.post('/fish-detection'), (req, res) => {
-    console.log("g")
+app.post('/fish-detection', (req, res) => {
+   
     const image = {
         source: { imageUri: req.body.downloadURL }
     }
 
-    client.labelDetection(image)
+    client.labelDetection(image.source.imageUri)
         .then(response => {
-            console.log('h')
-            let fishData = response[0].data
-            res.send(fishData)
+            const labels = response[0].labelAnnotations
+            // let fishData = response[0].data
+            res.send(labels)
         })
         .catch(err => {
             console.log("this is an error", err)
             res.sendStatus(500).send("We have encountered an error");
         })
-}
+})
 
 app.listen(PORT, _ => {
     console.log(`Express listening on ${PORT}, ctrl+c to kill.`)
