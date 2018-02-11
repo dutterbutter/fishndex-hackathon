c159fcaa1091fe4692759d0231577e115c03a799
Dustin Brickwood <dustinbrickwood204@gmail.com>
2018-02-10 22:23:59 -0500

    UI button in place

--- src/components/ButtonCamera.js
+++ src/components/ButtonCamera.js
@@ -1,47 +1,20 @@
 import React from 'react';
-import IconButton from 'material-ui/IconButton';
-import ActionCameraEnhance from 'material-ui/svg-icons/action/camera-enhance';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import PhotoAdd from 'material-ui/svg-icons/image/add-a-photo';
 
-const styles = {
-  smallIcon: {
-    width: 36,
-    height: 36,
-  },
-  mediumIcon: {
-    width: 48,
-    height: 48,
-  },
-  largeIcon: {
-    width: 60,
-    height: 60,
-  },
-  small: {
-    width: 72,
-    height: 72,
-    padding: 16,
-  },
-  medium: {
-    width: 96,
-    height: 96,
-    padding: 24,
-  },
-  large: {
-    width: 120,
-    height: 120,
-    padding: 30,
-  },
-};
+const buttonCamera = (props) => {
 
-const ButtonCamera = () => (
-  <div>
-    <IconButton
-      iconStyle={styles.smallIcon}
-      style={styles.small}
-    >
-      <ActionCameraEnhance />
+  const style = {
+    marginRight: 20,
+  };
 
-    </IconButton>
-  </div>
-);
+  return (
+    <FloatingActionButton style={style}>
+      <PhotoAdd>
+      <input type="file" name="image" accept="image/*" capture="user"/>
+      </PhotoAdd>
+    </FloatingActionButton>
+  )
+}
 
-export default ButtonCamera;
+export default buttonCamera;
