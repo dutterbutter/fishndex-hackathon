a06a0de903857b7808c3bce971acfe494bf53cfc
David Kajpust <kajpustd@gmail.com>
2018-02-10 22:23:52 -0500

    got wiki and google vision api working

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,16 +14,28 @@ class App extends Component {
     super()
     this.state = {
       fishData: [],
+      fishName: "",
+      fishDescription: []
     }
     const config = Deploy.configFire;
     firebase.initializeApp(config);
 
   }
 
+
+  sendVisionToOurDatabase = async () => {
+    let fishStateData = this.state.fishData;
+    console.log(fishStateData);
+    let correctFish = await axios.post('http://localhost:8080/checkFishName', { fishStateData });
+    console.log(correctFish);
+  }
+
+
+
   //we need to pass the BLOB (img) to this function
   //This will then pass this into firebase storage
   //which then will be sent to google vision API
-  visionUploadHandler = (acceptedFiles) => {
+  visionUploadHandler = async (acceptedFiles) => {
     let file = acceptedFiles;
     let storageRef = firebase.storage().ref('vision-images/' + file.name);
 
@@ -45,12 +57,40 @@ class App extends Component {
 
             this.setState({
               fishData: fishCopy
-            })
+            });
             console.log(this.state.fishData)
+            // return this.sendVisionToOurDatabase();
+
+            return axios.post('http://localhost:8080/checkFishName', { fishCopy });
+          }).then((response) => {
+            console.log(response.data);
+            let topTwentyResults = response.data[2];
+
+            for (let i = 0; i < topTwentyResults.length; i++) {
+              let searchForWordFish = topTwentyResults[i].search("fish")
+              console.log(topTwentyResults[i])
+              if (searchForWordFish !== -1) {
+                this.setState({
+                  fishDescription: topTwentyResults[i],
+                  fishName: response.data[0]
+                })
+                if(i === 19){
+                  
+                }
+                break;
+              }
+            }
+
+
+
+            // }).then((response)=>{
+            //   console.log(response);
           })
-          
+
       })
     console.log("Done. Enjoy.")
+
+
   }
 
   render() {
@@ -58,12 +98,13 @@ class App extends Component {
     return (
       <div className="container-fluid">
         <div className="d-flex flex-row">
-          fish dex
+          {this.state.fishName}
           </div>
-          <div className="d-flex justify-contents-center align-items-center border "> center this thing</div>
+        <div className="d-flex justify-contents-center align-items-center border "></div>
         <GoodCamera visionUploaderHandler={this.visionUploadHandler} />
-          <ButtonCamera className="mt-auto p-2"/>
-        </div>
+        <ButtonCamera className="mt-auto p-2" />
+        <div>{this.state.fishDescription}</div>
+      </div>
     );
   }
 }
--- src/server.js
+++ src/server.js
@@ -8,6 +8,11 @@ const admin = require("firebase-admin")
 const axios = require('axios');
 const deploy = require('./keys.js');
 const serviceAccount = deploy.firepack;
+const fishNameData = require('../fishNameData.json');
+// console.log(fishNameData[1].FIELD2);
+
+const fishTestData = require('../testFrontEndData.json');
+// console.log(fishTestData);
 
 app = express()
 app.use(express.static('public'))
@@ -20,8 +25,48 @@ admin.initializeApp({
     databaseURL: "https://fishdex-hackathon.firebaseio.com"
 });
 
+
+app.post('/checkFishName', async (req, res) => {
+
+    let visionApiInfo = req.body.fishCopy;
+    console.log(visionApiInfo);
+    // let visionApiInfo = fishTestData
+    let correctAnswer = "WRONG";
+
+    for (let i = 0; i < visionApiInfo.length; i++) {
+        // console.log(visionApiInfo[i].description.toUpperCase())
+        for (let j = 0; j < fishNameData.length; j++) {
+            if (visionApiInfo[i].description === 'fish') break;
+            let searchString = fishNameData[j].FIELD2.toUpperCase();
+            // console.log(searchString)
+            let didItMatch = searchString.search(visionApiInfo[i].description.toUpperCase())
+            // console.log(didItMatch);
+            if (didItMatch !== -1) {
+                correctAnswer = visionApiInfo[i].description;
+                break;
+            }
+        }
+        if (correctAnswer !== "WRONG") {
+            console.log(correctAnswer);
+            break;
+        }
+        console.log(correctAnswer);
+    }
+    let urlWiki = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + correctAnswer + "&limit=20"
+    axios.get(urlWiki)
+        .then(result => {
+            let wikiData = result.data;
+            console.log(wikiData);
+            res.send(wikiData);
+        })
+
+})
+
+
+
+
 app.post('/fish-detection', (req, res) => {
-   
+
     const image = {
         source: { imageUri: req.body.downloadURL }
     }
